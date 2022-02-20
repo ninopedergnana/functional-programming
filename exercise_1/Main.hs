3319402dd6a5702343063b1ae43dd6ab4532db4b
pedernin <pedernin@students.zhaw.ch>
2022-02-20 22:45:08 +0100

    fixed a mistake

--- exercise_1/Main.hs
+++ exercise_1/Main.hs
@@ -1,21 +1,24 @@
 import Data.List (sort)
 
--- [[1,2,3],[1,3,2], [1]] -> [1,2,3,1,3,2,1]
-
 -- Aufgabe 1
+-- flatSort [[1,2,3],[1,3,2], [1]] -> [1,2,3,1,3,2,1]
 flatSort :: [[Integer]] -> [Integer]
-flatSort = concat
+flatSort xs = concat $ map sort xs
 
 -- Aufgabe 2 
-
+-- concat [[3,2,1],[4]] -> [3,2,1,4]
+-- flatSort [[3,2,1], [4]] -> [1,2,3,4]
 
 -- Aufgabe 3
+-- flatSortRecursion [[1,2,3],[1,3,2], [1]] -> [1,2,3,1,3,2,1]
 flatSortRecursion :: [[Integer]] -> [Integer]
 flatSortRecursion [] = []
 flatSortRecursion [x] = x
 flatSortRecursion (x:xs) = x ++ flatSortRecursion xs
 
 -- Aufgabe 4
+-- initial "abc" "abcd" -> True
+-- initial "abc" "asdfabc" -> False
 initial :: String -> String -> Bool
 initial x y = 
     x == take z y
